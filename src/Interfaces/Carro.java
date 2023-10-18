b379bc01b8a7027a85f5b42d660cc5cd370070c1
IvandersonDev <ivandersonamaral16@gmail.com>
2023-10-18 11:04:02 -0300

    Initial commit

---
+++ src/Interfaces/Carro.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Carro {
+    void atualizar(String acao);
+
+    void frente();
+    void esquerda();
+    void direita();
+    void parar();
+}
---
+++ src/Methods/Viatura.java
@@ -0,0 +1,31 @@
+package Methods;
+
+import Interfaces.Carro;
+
+public class Viatura implements Carro {
+
+    @Override
+    public void atualizar(String acao) {
+        System.out.println("Viatura está seguindo o carro roubado. Ação da viatura: " + acao);
+    }
+
+    @Override
+    public void frente() {
+        System.out.println("Viatura está indo para frente.");
+    }
+
+    @Override
+    public void esquerda() {
+        System.out.println("Viatura está virando à esquerda.");
+    }
+
+    @Override
+    public void direita() {
+        System.out.println("Viatura está virando à direita.");
+    }
+
+    @Override
+    public void parar() {
+        System.out.println("Viatura parou.");
+    }
+}
